faba7e738f919d327f818b0168c8fe4132f91348
mrvsequeira <109689698+mrvsequeira@users.noreply.github.com>
2022-08-27 06:34:55 -0300

    Create create.users.sh

---
+++ create.users.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+ 
+echo "Dando inicio a criação do Script.sh uma Estrutura de Usuarios, Diretórios e Permissões." 
+
+echo "O comando abaixo irá criar os seguintes diretórios: o publico, adm, ven e sec."
+
+mkdir publico/ adm/ ven/ sec/
+
+echo "O comando abaixo irá criar os grupos: GRP_ADM, GRP_VEN e GRP_SEC."
+
+groupadd GRP_ADM
+groupadd GRP_VEN
+groupadd GRP_SEC
+
+echo "Agora daremos permissão total para o diretório publico."
+
+chmod 777 publico/
+
+echo "iniciando a criação dos usuários e adicionados nos seus respectivos grupos."
+echo "Foi utilizado a opção -6 como cryptografia, no qual é baseado em SHA512-based."
+
+echo "Criando usuários e adicionando no grupo GRP_ADM."
+
+useradd -c "Carlos da Silva" -s /bin/bash -m -G GRP_ADM -p $(openssl passwd -6 Senha123) carlos
+useradd -c "Maria da Silva" -s /bin/bash -m -G GRP_ADM -p $(openssl passwd -6 Senha123) maria
+useradd -c "Joao da Silva" -s /bin/bash -m -G GRP_ADM -p $(openssl passwd -6 Senha123) joao_
+
+echo "Criando usuários e adicionando no grupo GRP_VEN."
+
+useradd -c "Debora da Silva" -s /bin/bash -m -G GRP_VEN -p $(openssl passwd -6 Senha123) debora
+useradd -c "Sebastiana da Silva" -s /bin/bash -m -G GRP_VEN -p $(openssl passwd -6 Senha123) sebastiana
+useradd -c "Roberto da Silva" -s /bin/bash -m -G GRP_VEN -p $(openssl passwd -6 Senha123) roberto
+
+echo "Criando usuários e adicionando no grupo GRP_SEC."
+
+useradd -c "Josefina da Silva" -s /bin/bash -m -G GRP_SEC -p $(openssl passwd -6 Senha123) josefina
+useradd -c "Amanda da Silva" -s /bin/bash -m -G GRP_SEC -p $(openssl passwd -6 Senha123) amanda
+useradd -c "Rogerio da Silva" -s /bin/bash -m -G GRP_SEC -p $(openssl passwd -6 Senha123) rogerio
+
+echo "Dando permissão total ao dono root, dando permissão total aos usuarios do grupo e excluindo a permissão de leitura, escrita e execução de outros."
+
+chmod 770 adm/ ven/ sec/
